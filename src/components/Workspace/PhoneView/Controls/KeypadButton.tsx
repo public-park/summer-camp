16f6f06a5421b9328d26a45971d4b4fa5d61cd79
Noah Park <polygon.puma@gmail.com>
2020-06-02 08:15:06 +0200

    fixed keypad button component

--- src/components/Workspace/PhoneView/Controls/KeypadButton.tsx
+++ src/components/Workspace/PhoneView/Controls/KeypadButton.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export const KeypadButton = (props: any) => {
+interface KeypadButtonProps {
+  addDigit: (digit: string) => void;
+  number: string;
+  letter: string;
+}
+
+export const KeypadButton = (props: KeypadButtonProps) => {
   return (
     <button onClick={() => props.addDigit(props.number)} className="digit-button" key={`digit_${props.number}`}>
       <div className="number">{props.number}</div>
-      <span className="letter"></span> {props.letters}
+      <span className="letter">{props.letter}</span>
     </button>
   );
 };

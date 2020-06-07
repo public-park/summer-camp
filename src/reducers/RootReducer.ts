c51596f0e117fa0ae3a392d9aa4eb2ba0b43ba63
Noah Park <polygon.puma@gmail.com>
2020-06-07 23:16:24 +0200

    refactor of store reset upon logout

--- src/reducers/RootReducer.ts
+++ src/reducers/RootReducer.ts
@@ -2,8 +2,6 @@ import { Store } from '../store/Store';
 import produce from 'immer';
 import { Action } from '../actions/ActionType';
 import { DefaultStore } from '../store/DefaultStore';
-import { UserConnectionState } from '../models/enums/UserConnectionState';
-import { UserActivity } from '../models/enums/UserActivity';
 
 const reducer = (state: Store = DefaultStore, action: Action): Store => {
   return produce(state, (draft) => {
@@ -43,6 +41,13 @@ const reducer = (state: Store = DefaultStore, action: Action): Store => {
 
       case 'PHONE_TOKEN_UPDATED':
         draft.phone.token = action.payload.token;
+
+        if (action.payload.error) {
+          draft.phone.error = action.payload.error;
+        } else {
+          draft.phone.error = undefined;
+        }
+
         break;
 
       case 'PHONE_CONFIGURATION_UPDATED':
@@ -68,26 +73,14 @@ const reducer = (state: Store = DefaultStore, action: Action): Store => {
         break;
 
       case 'USER_LOGOUT':
+        Object.assign(draft, DefaultStore);
+
         if (action.payload.reason) {
           draft.page = 'LOGOUT_PAGE';
           draft.logout.reason = action.payload.reason;
         } else {
           draft.page = 'LOGIN_PAGE';
         }
-
-        draft.phone.token = undefined;
-        draft.phone.state = 'OFFLINE';
-        draft.phone.configuration = undefined;
-        draft.workspace.view = 'FETCH_CONFIGURATION_VIEW';
-        draft.call = undefined;
-        draft.token = undefined;
-
-        draft.user.id = '';
-        draft.user.name = '';
-        draft.user.profileImageUrl = undefined;
-        draft.user.labels = [];
-        draft.user.activity = UserActivity.Unknown;
-        draft.user.connectionState = UserConnectionState.Closed;
         break;
     }
   });

6fecf9f823a91af114384844e71eee7cc90656cd
Noah Park <polygon.puma@gmail.com>
2020-06-12 11:01:54 +0200

    add page lifecycle React hook

---
+++ src/hooks/usePageLifecycle.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+
+let lastCheckInSeconds: number;
+const intervalInSeconds: number = 15;
+const gracePeriodInSeconds: number = 4;
+
+export const usePageLifecycle = () => {
+  const [isOnline, setIsOnline] = useState(false);
+  const [isResume, setIsResume] = useState(false);
+
+  const updateBrowserOnlineState = (event: Event) => {
+    const state = navigator.onLine ? 'online' : 'offline';
+
+    console.log('Browser Event: ' + event.type + '; state: ' + state);
+
+    setIsOnline(state === 'online');
+  };
+
+  const getTimeInSeconds = () => {
+    return Math.floor(new Date().getTime() / 1000);
+  };
+
+  const checkIfResume = (lastCheckInSeconds: number) => {
+    if (getTimeInSeconds() > lastCheckInSeconds + intervalInSeconds + gracePeriodInSeconds) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const getMinutes = (seconds: number) => {
+    return Math.floor(seconds / 60);
+  };
+
+  useEffect(() => {
+    window.addEventListener('online', updateBrowserOnlineState);
+    window.addEventListener('offline', updateBrowserOnlineState);
+
+    setInterval(function () {
+      if (checkIfResume(lastCheckInSeconds)) {
+        const elapsedInSeconds = getTimeInSeconds() - lastCheckInSeconds;
+
+        console.log(
+          `resume, time shift is ${getMinutes(elapsedInSeconds)} minute(s), ${
+            elapsedInSeconds - getMinutes(elapsedInSeconds) * 60
+          } second(s)`
+        );
+
+        setIsResume(true);
+      } else {
+        setIsResume(false);
+      }
+
+      lastCheckInSeconds = getTimeInSeconds();
+    }, intervalInSeconds * 1000);
+  }, []);
+
+  return { isResume, isOnline };
+};

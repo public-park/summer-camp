d07646bf2892284e7dbea051b9228a315e5e7f39
Noah Park <polygon.puma@gmail.com>
2020-06-19 07:53:24 +0200

    fix allowAccessWith middleware

--- src-server/middlewares/allowAccessWith.ts
+++ src-server/middlewares/allowAccessWith.ts
@@ -1,9 +1,9 @@
 import { Response, NextFunction } from 'express';
-import { UserPermissions } from '../models/UserPermissions';
 import { RequestWithUser } from '../requests/RequestWithUser';
 import { UserNotAuthorizedException } from '../exceptions/UserNotAuthorizedException';
+import { Permission } from '../models/roles/Permission';
 
-export default function allowAccessWith(name: UserPermissions) {
+export default function allowAccessWith(name: Permission) {
   return (request: RequestWithUser, response: Response, next: NextFunction) => {
     if (request.user && request.user.hasPermission(name)) {
       return next();

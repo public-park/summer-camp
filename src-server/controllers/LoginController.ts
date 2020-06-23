d8b46bfc86515325b11e026ca32f92f08777eff8
Noah Park <polygon.puma@gmail.com>
2020-06-23 08:32:19 +0200

    return the same HTTP response code if user not found or password invalid

--- src-server/controllers/LoginController.ts
+++ src-server/controllers/LoginController.ts
@@ -1,19 +1,18 @@
 import { Request, Response } from 'express';
 import { TokenHelper } from '../helpers/TokenHelper';
 import { userRepository, authenticationProvider } from '../worker';
-import { UserNotFoundError } from '../repository/UserNotFoundError';
 import { log } from '../logger';
 
 const login = async (req: Request, res: Response, next: any) => {
   log.info(`get user by name: ${req.body.name}`);
 
-  res.header('Access-Control-Allow-Origin: *');
-
   try {
     const user = await userRepository.getByName(req.body.name);
 
     if (!user) {
-      throw new UserNotFoundError();
+      /* do not expose a dedicated error code if user is not found */
+      res.status(401).end();
+      return;
     }
 
     const isValidPassword = await authenticationProvider.authenticate(user, req.body.password);
@@ -22,6 +21,7 @@ const login = async (req: Request, res: Response, next: any) => {
       const payload = {
         token: TokenHelper.createJwt(user, 14400),
         userId: user.id,
+        accountId: user.accountId,
       };
 
       res.json(payload);

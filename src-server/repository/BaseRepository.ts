fd6e051b214284ee8256d9d7b4dbcc43997420e7
Noah Park <polygon.puma@gmail.com>
2020-07-24 10:17:16 +0200

    remove getAll from base repository

--- src-server/repository/BaseRepository.ts
+++ src-server/repository/BaseRepository.ts
@@ -1,6 +1,7 @@
+import { Account } from '../models/Account';
+
 export interface BaseRepository<T> {
   getById: (id: string) => Promise<T | undefined>;
-  getAll: (offset: number, limit: number) => Promise<Array<T>>;
   create: (...args: any) => Promise<T>;
   update: (entity: T) => Promise<T>;
   delete: (entity: T) => Promise<void>;

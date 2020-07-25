7924b3aae4d79636d3d27ff21b14cfa9e6adef91
Noah Park <polygon.puma@gmail.com>
2020-07-25 08:36:57 +0200

    add input and output device, create call returns promise

--- src/phone/PhoneControl.ts
+++ src/phone/PhoneControl.ts
@@ -3,11 +3,13 @@ import { PhoneState } from './PhoneState';
 
 export interface PhoneControl {
   init: (token: string) => void;
-  call: (phoneNumber: string) => Call;
+  call: (phoneNumber: string) => Promise<Call>;
   getState: () => PhoneState;
   destroy: () => void;
   onStateChanged: (listener: (state: PhoneState) => void) => void;
   onError: (listener: (error: Error) => void) => void;
   onIncomingCall: (listener: (call: Call) => void) => void;
   onOutgoingCall: (listener: (call: Call) => void) => void;
+  setInputDevice: (deviceId: string) => Promise<void>;
+  setOutputDevice: (deviceId: string) => Promise<void>;
 }

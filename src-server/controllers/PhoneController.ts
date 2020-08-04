1f439c059b79c42808e1579904fb6c108ab2697b
Noah Park <polygon.puma@gmail.com>
2020-08-04 07:41:48 +0200

    remove account from controller

--- src-server/controllers/PhoneController.ts
+++ src-server/controllers/PhoneController.ts
@@ -1,19 +1,11 @@
 import { Response, NextFunction } from 'express';
-import { accountRepository } from '../worker';
 import { RequestWithUser } from '../requests/RequestWithUser';
 import { createVoiceToken } from '../helpers/twilio/TwilioHelper';
-import { AccountNotFoundError } from '../repository/AccountNotFoundError';
 
 export const createToken = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
-    const account = await accountRepository.getById(req.user.accountId); // TODO rename to accounts
-
-    if (!account) {
-      throw new AccountNotFoundError();
-    }
-
     const payload = {
-      token: createVoiceToken(account, req.user, 3600),
+      token: createVoiceToken(req.user, 3600),
     };
 
     return res.json(payload);

36167606067e0daa46de15083e898e6dfaec4ced
Noah Park <polygon.puma@gmail.com>
2020-08-14 10:41:21 +0200

    add call controller

---
+++ src-server/controllers/CallController.ts
@@ -0,0 +1,27 @@
+import { Response, NextFunction } from 'express';
+import { callRepository } from '../worker';
+import { RequestWithUser } from '../requests/RequestWithUser';
+import { RequestWithCall } from '../requests/RequestWithCall';
+
+const fetch = async (req: RequestWithCall, res: Response, next: NextFunction) => {
+  try {
+    res.json(req.call.toResponse());
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const getAll = async (req: RequestWithUser, res: Response, next: any) => {
+  try {
+    const calls = await callRepository.getByUser(req.user, 0, 50);
+
+    res.json(calls);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const CallController = {
+  fetch,
+  getAll,
+};

d3c559329275dd9f570abdba02c7337e87dc6517
Noah Park <polygon.puma@gmail.com>
2020-08-22 11:14:43 +0200

    add call hold to Twilio helper

--- src-server/helpers/twilio/TwilioHelper.ts
+++ src-server/helpers/twilio/TwilioHelper.ts
@@ -6,16 +6,39 @@ import { ServerException } from '../../exceptions/ServerException';
 import { ConfigurationNotFoundException } from '../../exceptions/ConfigurationNotFoundException';
 import { InvalidConfigurationException } from '../../exceptions/InvalidConfigurationException';
 import { log } from '../../logger';
+import { AccountConfiguration } from '../../models/AccountConfiguration';
+import { ParticipantListInstanceCreateOptions } from 'twilio/lib/rest/api/v2010/account/conference/participant';
+import { CallConnectionType } from '../../models/CallConnectionType';
+import { Call } from '../../models/Call';
+import { RequestWithAccount } from '../../requests/RequestWithAccount';
+import { getCallStatusEventUrl } from '../../controllers/callback/PhoneHelper';
+import { CallNotFoundException } from '../../exceptions/CallNotFoundException';
 
 interface VoiceAccessToken extends AccessToken {
   identity: string;
 }
 
+interface TwilioHelperOptions {
+  edge: string;
+}
+
 export class TwilioHelper {
   client: Twilio;
 
-  constructor(key: string, secret: string, accountSid: string) {
+  // TODO add edge to configuration, env param TWILIO_API_EDGE=frankfurt
+  constructor(account: Account, options?: TwilioHelperOptions) {
+    if (!account.hasConfiguration()) {
+      throw new ConfigurationNotFoundException();
+    }
+
+    const { key, secret, accountSid } = account.configuration as AccountConfiguration;
+
+    if (!key || !secret || !accountSid) {
+      throw new InvalidConfigurationException();
+    }
+
     this.client = new Twilio(key, secret, {
+      ...options,
       accountSid: accountSid,
       edge: 'frankfurt',
     });
@@ -88,41 +111,111 @@ export class TwilioHelper {
 
     return sid;
   }
+
+  getCallConnectionType = (endpoint: string): CallConnectionType => {
+    if (endpoint.startsWith('client:')) {
+      return CallConnectionType.WebRTC;
+    }
+
+    if (endpoint.startsWith('sip:')) {
+      return CallConnectionType.SIP;
+    }
+
+    if (endpoint.startsWith('+')) {
+      return CallConnectionType.PhoneNumber;
+    }
+
+    throw new Error();
+  };
+
+  async getCall(callSid: string) {
+    return await this.client.calls(callSid).fetch();
+  }
+
+  async addUserToConference(request: RequestWithAccount, call: Call) {
+    await this.addParticipantToConference(
+      call.id,
+      call.to,
+      `client:${getIdentityByUserId(<string>call.userId)}`,
+      'agent',
+      getCallStatusEventUrl(request, call)
+    );
+  }
+
+  async addCustomerToConference(request: RequestWithAccount, call: Call) {
+    await this.addParticipantToConference(
+      call.id,
+      call.from,
+      call.to,
+      'customer',
+      getCallStatusEventUrl(request, call)
+    );
+  }
+
+  async addParticipantToConference(
+    conferenceName: string,
+    from: string,
+    to: string,
+    label: string,
+    statusEventUrl: string
+  ): Promise<string> {
+    let options: ParticipantListInstanceCreateOptions = {
+      to: to,
+      from: from,
+      earlyMedia: true,
+      endConferenceOnExit: true,
+      label: label,
+      timeout: 45,
+      statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed'],
+      statusCallbackMethod: 'POST',
+      statusCallback: statusEventUrl,
+    };
+
+    const call = await this.client.conferences(conferenceName).participants.create(options);
+
+    return call.callSid;
+  }
+
+  async holdParticipant(conferenceName: string, label: string, hold: boolean): Promise<void> {
+    const conferences = await this.client.conferences.list({
+      friendlyName: conferenceName,
+      status: 'in-progress',
+      limit: 1,
+    });
+
+    if (!conferences[0]) {
+      throw new CallNotFoundException();
+    }
+
+    await this.client.conferences(conferences[0].sid).participants(label).update({ hold: hold });
+  }
 }
 
-export const createVoiceToken = (account: Account, user: User, lifetime: number = 600): string => {
-  if (!account.configuration) {
+export const createVoiceToken = (user: User, lifetime: number = 600): string => {
+  if (!user.account.hasConfiguration()) {
     throw new ConfigurationNotFoundException();
   }
 
-  if (
-    !account.configuration.key ||
-    !account.configuration.secret ||
-    !account.configuration.accountSid ||
-    !account.configuration.applicationSid
-  ) {
+  const { key, secret, accountSid } = user.account.configuration as AccountConfiguration;
+
+  if (!key || !secret || !accountSid) {
     throw new InvalidConfigurationException();
   }
 
-  const accessToken = new AccessToken(
-    account.configuration.accountSid,
-    account.configuration.key,
-    account.configuration.secret,
-    {
-      ttl: lifetime,
-    }
-  );
+  const accessToken = new AccessToken(accountSid, key, secret, {
+    ttl: lifetime,
+  });
 
   log.info(`create phone token for ${user.id} (${user.name})`);
 
   const options: AccessToken.VoiceGrantOptions = {};
 
-  if (account.configuration.inbound.isEnabled) {
+  if (user.account.configuration && user.account.configuration.inbound.isEnabled) {
     options.incomingAllow = true;
   }
 
-  if (account.configuration.outbound.isEnabled) {
-    options.outgoingApplicationSid = account.configuration.applicationSid;
+  if (user.account.configuration && user.account.configuration.outbound.isEnabled) {
+    options.outgoingApplicationSid = user.account.configuration.applicationSid;
   }
 
   const grant = new AccessToken.VoiceGrant(options);
@@ -138,4 +231,12 @@ export const createVoiceToken = (account: Account, user: User, lifetime: number 
 
 export const getIdentity = (user: User): string => {
   return user.id.split('-').join('_');
+};
+
+export const getIdentityByUserId = (userId: string): string => {
+  return userId.split('-').join('_');
+};
+
+export const convertIdentityToUserId = (identity: string): string => {
+  return identity.replace('client:', '').split('_').join('-');
 };

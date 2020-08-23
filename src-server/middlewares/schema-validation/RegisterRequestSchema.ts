6b8509583e2ad7b744cce2ae74bed3695c678709
Noah Park <polygon.puma@gmail.com>
2020-08-23 09:18:12 +0200

    set min lenght to 3 characters

--- src-server/middlewares/schema-validation/RegisterRequestSchema.ts
+++ src-server/middlewares/schema-validation/RegisterRequestSchema.ts
@@ -2,7 +2,7 @@ export const RegisterRequestSchema = {
   type: 'object',
   properties: {
     name: { type: 'string', minLength: 3, maxLength: 40 },
-    password: { type: 'string', minLength: 3, maxLength: 40 },
+    password: { type: 'string', minLength: 1, maxLength: 40 },
   },
   required: ['name', 'password'],
 

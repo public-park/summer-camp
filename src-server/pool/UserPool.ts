498d48a4fc9cbd032f4902793d95bbe941a8bb85
Noah Park <polygon.puma@gmail.com>
2020-08-24 17:58:34 +0200

    update user pool and model

--- src-server/pool/UserPool.ts
+++ src-server/pool/UserPool.ts
@@ -3,10 +3,7 @@ import { UserRepository } from '../repository/UserRepository';
 import { UserNotFoundException } from '../exceptions/UserNotFoundException';
 import { User } from '../models/User';
 import { Account } from '../models/Account';
-
-const getUserWithOnlineState = (user: User): UserWithOnlineState => {
-  return new UserWithOnlineState(user, false);
-};
+import { WebSocketWithKeepAlive } from '../WebSocketWithKeepAlive';
 
 export class UserPool {
   users: Map<string, UserWithOnlineState>;
@@ -17,45 +14,87 @@ export class UserPool {
     this.repository = repository;
   }
 
-  async getUserById(id: string): Promise<UserWithOnlineState | undefined> {
-    if (this.users.has(id)) {
-      return Promise.resolve(this.users.get(id));
-    } else {
-      const user = await this.repository.getById(id);
+  async add(account: Account, id: string, socket?: WebSocketWithKeepAlive | undefined): Promise<UserWithOnlineState> {
+    const user = await this.getByIdWithFallback(account, id);
 
-      if (!user) {
-        throw new UserNotFoundException();
-      }
+    if (!user) {
+      throw new UserNotFoundException();
+    }
 
-      return getUserWithOnlineState(user);
+    if (socket) {
+      user.sockets.add(socket);
+    }
+
+    this.users.set(user.id, user);
+
+    return user;
+  }
+
+  update(user: User) {
+    let existing = this.getById(user.id);
+
+    if (existing) {
+      this.users.set(user.id, this.getUserWithOnlineState(user, existing.sockets.get()));
     }
   }
 
-  async getFirstByAccount(account: Account): Promise<UserWithOnlineState | undefined> {
-    const users = this.getOnlineByAccount(account);
+  delete(id: string) {
+    const user = this.getById(id);
+
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+
+    this.users.delete(user.id);
+  }
+
+  getById(id: string): UserWithOnlineState | undefined {
+    return this.users.get(id);
+  }
+
+  async getByIdWithFallback(account: Account, id: string): Promise<UserWithOnlineState | undefined> {
+    let userWithOnlineState = this.getById(id);
+
+    if (userWithOnlineState) {
+      return userWithOnlineState;
+    } else {
+      const user = await this.repository.getById(account, id);
+
+      if (user) {
+        return this.getUserWithOnlineState(user);
+      }
+    }
+  }
+
+  async getOneByAccount(account: Account): Promise<UserWithOnlineState | undefined> {
+    const users = this.getAll(account);
 
     if (users.length !== 0) {
       return Promise.resolve(users[0]);
+    }
+  }
+
+  async getOneByAccountWithFallback(account: Account): Promise<UserWithOnlineState | undefined> {
+    const user = this.getOneByAccount(account);
+
+    if (user) {
+      return Promise.resolve(user);
     } else {
-      const user = await this.repository.getOneByAccountId(account.id);
+      const user = await this.repository.getOneByAccount(account);
 
       if (!user) {
         throw new UserNotFoundException();
       }
 
-      return getUserWithOnlineState(user);
+      return this.getUserWithOnlineState(user);
     }
   }
 
-  getOnlineByAccount(account: Account): Array<UserWithOnlineState> {
-    const list = new Array<UserWithOnlineState>();
-
-    for (let user of this.users.values()) {
-      if (user.accountId === account.id) {
-        list.push(user);
-      }
-    }
-
-    return list;
+  getAll(account: Account): Array<UserWithOnlineState> {
+    return Array.from(this.users.values()).filter((user) => user.account.id === account.id);
   }
+
+  getUserWithOnlineState = (user: User, sockets: Array<WebSocketWithKeepAlive> = []): UserWithOnlineState => {
+    return new UserWithOnlineState(user, sockets, this.repository);
+  };
 }
--- src-server/pool/UserWithOnlineState.ts
+++ src-server/pool/UserWithOnlineState.ts
@@ -1,28 +1,114 @@
 import { UserActivity } from '../models/UserActivity';
-import { User } from '../models/User';
+import { User, UserResponse } from '../models/User';
+import { Call } from '../models/Call';
+import { UserRepository } from '../repository/UserRepository';
+import { WebSocketWithKeepAlive } from '../WebSocketWithKeepAlive';
+import { CallStatus } from '../models/CallStatus';
+
+export class UserSockets {
+  private sockets: Array<WebSocketWithKeepAlive>;
+
+  constructor(sockets: Array<WebSocketWithKeepAlive> = []) {
+    this.sockets = sockets;
+  }
+
+  add(socket: WebSocketWithKeepAlive) {
+    this.sockets.push(socket);
+  }
+
+  remove(socket: WebSocketWithKeepAlive) {
+    this.sockets = this.sockets.filter((s) => s !== socket);
+  }
+
+  broadcast(payload: string) {
+    this.sockets.forEach((sockets) => {
+      sockets.send(payload);
+    });
+  }
+
+  get() {
+    return this.sockets;
+  }
+
+  length() {
+    return this.sockets.length;
+  }
+}
+
+export interface UserWithOnlineStateResponse extends UserResponse {
+  call: {
+    id: string;
+    from: string;
+    to: string;
+    status: CallStatus | undefined;
+  } | null;
+  sockets: number;
+}
 
 export class UserWithOnlineState extends User {
-  isOnACall: boolean;
-  isOnline: boolean;
+  call: Call | undefined;
+  users: UserRepository;
+  sockets: UserSockets;
 
-  constructor(user: User, isOnline: boolean) {
+  constructor(user: User, sockets: Array<WebSocketWithKeepAlive>, users: UserRepository) {
     super(
       user.id,
       user.name,
       user.profileImageUrl,
       user.tags,
       user.activity,
-      user.accountId,
+      user.account,
       user.authentication,
       user.role,
       user.createdAt
     );
 
-    this.isOnACall = false;
-    this.isOnline = isOnline;
+    this.call = undefined;
+    this.users = users;
+    this.sockets = new UserSockets(sockets);
   }
 
   get isAvailable(): boolean {
-    return this.activity === UserActivity.WaitingForWork && !this.isOnACall;
+    return this.activity === UserActivity.WaitingForWork && !this.call;
+  }
+
+  async persist(): Promise<void> {
+    await this.users.update(this.account, this);
+  }
+
+  toResponse(): UserWithOnlineStateResponse {
+    const response = super.toResponse() as UserWithOnlineStateResponse;
+
+    if (this.call) {
+      response.call = { id: this.call.id, from: this.call.from, to: this.call.to, status: this.call.status };
+    } else {
+      response.call = null;
+    }
+
+    response.sockets = this.sockets.length();
+
+    return response;
+  }
+
+  updateCallAndBroadcast(call: Call) {
+    this.call = call;
+
+    if (!call.isActive()) {
+      this.call = undefined;
+    }
+
+    this.broadcast({ call: this.toResponse().call });
+  }
+
+  updateCall(call: Call) {
+    this.call = call;
+
+    if (!call.isActive()) {
+      this.call = undefined;
+    }
+  }
+
+  broadcast(payload: object) {
+    this.sockets.broadcast(JSON.stringify(payload));
   }
 }

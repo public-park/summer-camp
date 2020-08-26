5a19168bf5df11e36cc4721653f523c71f97edca
Noah Park <polygon.puma@gmail.com>
2020-08-26 08:21:38 +0200

    alert on lost audio device, add device hook to workspace

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,47 +1,84 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { ConnectionLostAlert } from './NotificationLayer/ConnectionLostAlert';
 import { Header } from './Header/Header';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectWorkspaceView, selectConnectionState, selectWorkspaceNotification } from '../../store/Store';
+import {
+  selectWorkspaceView,
+  selectConnectionState,
+  selectWorkspaceNotification,
+  selectPhoneInputDevice,
+  selectPhoneOutputDevice,
+  selectAudioInputDevices,
+  selectAudioOutputDevices,
+} from '../../store/Store';
 import { PhoneView } from './PhoneView/PhoneView';
 import { UserConnectionState } from '../../models/enums/UserConnectionState';
 import { HeaderThemeProvider } from './Header/HeaderThemeProvider';
 import { WorkspaceView } from '../../actions/WorkspaceViewAction';
-import { ApplicationContext } from '../../context/ApplicationContext';
-import { fetchUserConfiguration } from './ConfigurationView/services/fetchUserConfiguration';
 import { ConnectView } from './ConnectView/ConnectView';
 import { CallHistoryView } from './CallHistoryView/CallHistoryView';
 import { ConfigurationView } from './ConfigurationView/ConfigurationView';
 import { ConfigurationContextProvider } from './ConfigurationView/ConfigurationContextProvider';
-import { setPhoneConfiguration } from '../../actions/PhoneAction';
+import { lostPhoneInputDevice, lostPhoneOutputDevice } from '../../actions/PhoneAction';
 import { AudioDeviceView } from './AudioDeviceView/AudioDeviceView';
 import { NotificationLayer } from './NotificationLayer/NotificationLayer';
+import { useReconnectWebSocket } from '../../hooks/useReconnectWebSocket';
+import { ConnectionLostWithReconnectAlert } from './NotificationLayer/ConnectionLostWithReconnectAlert';
+import { useAudioDevices } from '../../hooks/useAudioDevices';
+import { setAudioDevicesException, updateAudioDevices } from '../../actions/AudioDeviceAction';
+
+const hasLostDevice = (deviceId: string | undefined, devices: MediaDeviceInfo[] | undefined) => {
+  if (!deviceId || !devices) {
+    return false;
+  }
+
+  return !devices.find((device) => device.deviceId === deviceId);
+};
 
 export const Workspace = () => {
-  const { user } = useContext(ApplicationContext);
-
-  const connectionState = useSelector(selectConnectionState);
+  const state = useSelector(selectConnectionState);
   const view = useSelector(selectWorkspaceView);
   const notification = useSelector(selectWorkspaceNotification);
+  const phoneInputDevice = useSelector(selectPhoneInputDevice);
+  const phoneOutputDevice = useSelector(selectPhoneOutputDevice);
+  const audioInputDevices = useSelector(selectAudioInputDevices);
+  const audioOutputDevices = useSelector(selectAudioOutputDevices);
+
+  const { devices, exception } = useAudioDevices();
+  const { timer } = useReconnectWebSocket();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const init = async () => {
-      let configuration;
-
-      try {
-        configuration = await fetchUserConfiguration(user);
-      } catch {
-      } finally {
-        dispatch(setPhoneConfiguration(configuration));
-      }
-    };
-
-    if (view === 'FETCH_CONFIGURATION_VIEW' && connectionState === UserConnectionState.Open) {
-      init();
+    if (exception) {
+      dispatch(setAudioDevicesException(exception));
     }
-  }, [view, connectionState]);
+  }, [exception]);
+
+  useEffect(() => {
+    if (devices) {
+      console.debug('phone input/audio devices changed');
+
+      dispatch(
+        updateAudioDevices({
+          input: devices.input,
+          output: devices.output,
+        })
+      );
+    }
+  }, [devices]);
+
+  useEffect(() => {
+    if (hasLostDevice(phoneInputDevice, audioInputDevices)) {
+      dispatch(lostPhoneInputDevice('Your active microphone device was removed.'));
+    }
+  }, [phoneInputDevice, audioInputDevices]);
+
+  useEffect(() => {
+    if (hasLostDevice(phoneOutputDevice, audioOutputDevices)) {
+      dispatch(lostPhoneOutputDevice('Your active speaker device was removed.'));
+    }
+  }, [phoneOutputDevice, audioOutputDevices]);
 
   const getWorkspaceView = (view: WorkspaceView) => {
     switch (view) {
@@ -60,8 +97,7 @@ export const Workspace = () => {
 
       case 'AUDIO_DEVICE_VIEW':
         return <AudioDeviceView />;
-
-      case 'FETCH_CONFIGURATION_VIEW':
+      case 'CONNECT_VIEW':
         return <ConnectView />;
     }
   };
@@ -69,13 +105,13 @@ export const Workspace = () => {
   return (
     <div className="page-body">
       <div className="workspace">
-        {connectionState === UserConnectionState.Closed && <ConnectionLostAlert />}
+        {timer && <ConnectionLostWithReconnectAlert />}
+        {state === UserConnectionState.Expired && <ConnectionLostAlert />}
         {notification && <NotificationLayer />}
 
         <HeaderThemeProvider>
           <Header />
         </HeaderThemeProvider>
-
         {getWorkspaceView(view)}
       </div>
     </div>

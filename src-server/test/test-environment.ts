8c606ac2475fa49e8b6e5bc5f79ef95d0da59051
Noah Park <polygon.puma@gmail.com>
2020-08-29 17:09:57 +0200

    update test environment settings

--- src-server/test/test-environment.ts
+++ src-server/test/test-environment.ts
@@ -10,7 +10,7 @@ import { FileCallRepository } from '../repository/file/FileCallRepository';
 import { MongoCallRepository } from '../repository/mongo/MongoCallRepository';
 import CallModel from '../repository/mongo/CallSchema';
 
-/* MongoDB   */
+/* MongoDB */
 const uri = `mongodb://localhost:27017/summer-camp-test`;
 
 const mongoOptions = {
@@ -21,7 +21,7 @@ const mongoOptions = {
 };
 
 export const accountRepository = new MongoAccountRepository();
-export const userRepository = new MongoUserRepository();
+export const userRepository = new MongoUserRepository(accountRepository);
 export const callRepository = new MongoCallRepository();
 export const authenticationProvider = new PasswordAuthenticationProvider();
 
@@ -41,11 +41,11 @@ export const init = () => {
   });
 };
 
-/* File Storage
-export const accountRepository = new FileAccountRepository('./src-server/test/accounts-test.json');
-export const userRepository = new FileUserRepository('./src-server/test/users-test.json', accountRepository);
-export const callRepository = new FileCallRepository('./src-server/test/calls-test.json');
+/* File Storage 
 
+export const accountRepository = new FileAccountRepository('./src-server/test/accounts-test.json');
+export const userRepository = new FileUserRepository(accountRepository, './src-server/test/users-test.json');
+export const callRepository = new FileCallRepository('./src-server/test/calls-test.json');
 export const authenticationProvider = new PasswordAuthenticationProvider();
 
 export const init = () => {
@@ -57,7 +57,8 @@ export const init = () => {
     await callRepository.empty();
   });
 };
- */
+
+*/
 
 export const corporations = {
   mom: 'MomCorp',
@@ -72,6 +73,8 @@ export const personas = {
   joe: 'Joe the Engineer',
   max: 'Max the Astronaut',
   jane: 'Jane the Engineer',
+  tim: 'Tim the Explorer',
+  julia: 'Julia the Entrepreneur',
 };
 
 export const secret = 'my-secret-password';

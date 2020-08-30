8effd1690a412a050bab82e66b91eb37ea5ddb2f
Noah Park <polygon.puma@gmail.com>
2020-08-30 20:10:51 +0200

    remove express header globally

--- src-server/middlewares/setHeaders.ts
+++ src-server/middlewares/setHeaders.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 export const setHeaders = (request: Request, response: Response, next: NextFunction) => {
   response.setHeader('Magic', 'true');
   response.setHeader('Creator', 'Unicorn');
-  response.setHeader('X-Powered-By', 'none');
 
   next();
 };

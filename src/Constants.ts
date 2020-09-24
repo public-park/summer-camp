f35856ab4d5cd39466feb22594a0a0e6e92a2780
Noah Park <polygon.puma@gmail.com>
2020-09-24 16:16:19 +0200

    remove modifier from phone number check regex

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
-export const IS_PHONE_NUMBER_REGEXP = /^\+\d+$/g;
-export const IS_NUMBER_REGEXP = /^\d+$/g;
+export const IS_PHONE_NUMBER_REGEXP = /^\+\d+$/;
+export const IS_NUMBER_REGEXP = /^\d+$/;
 export const IS_TWILIO_ACCOUNT_SID_REGEXP = /^AC[0-9abcdef]{32}$/;
 export const IS_TWILIO_API_SECRET_REGEXP = /^[a-z0-9]{32}$/i;
 export const IS_TWILIO_API_KEY_REGEXP = /^SK[0-9abcdef]{32}$/;

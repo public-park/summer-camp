0c00934e80c2eedf6e4a36af7e857b53aa254a52
Noah Park <polygon.puma@gmail.com>
2020-09-25 07:37:44 +0200

    remove user lookup

--- src-server/controllers/callback/CallStatusEventController.ts
+++ src-server/controllers/callback/CallStatusEventController.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Response } from 'express';
-import { pool, callRepository as calls } from '../../worker';
+import { callRepository as calls } from '../../worker';
 import { getStatus, getDuration } from './CallStatusEventHelper';
 import { StatusCallbackRequest } from '../../requests/StatusCallbackRequest';
 
 const handleInbound = async (request: StatusCallbackRequest, response: Response, next: NextFunction) => {
   try {
-    const user = pool.getByIdOrFail(<string>request.call.userId);
-
     request.call.status = getStatus(request);
 
     await calls.update(request.call);
@@ -19,8 +17,6 @@ const handleInbound = async (request: StatusCallbackRequest, response: Response,
 
 const handleOutbound = async (request: StatusCallbackRequest, response: Response, next: NextFunction) => {
   try {
-    const user = pool.getByIdOrFail(<string>request.call.userId);
-
     request.call.callSid = request.body.CallSid;
     request.call.status = getStatus(request);
     request.call.duration = getDuration(request);

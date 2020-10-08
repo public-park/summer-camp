03239e0d258e3fca9042c0b260dffe54e0a8d55c
Noah Park <polygon.puma@gmail.com>
2020-10-08 07:24:09 +0200

    validate password min and max length

--- src-server/controllers/RegisterControllerValidator.ts
+++ src-server/controllers/RegisterControllerValidator.ts
@@ -1,8 +1,13 @@
+import { MAXIMUM_LENGTH_OF_USER_PASSWORD, MINIMUM_LENGTH_OF_USER_PASSWORD } from '../Constants';
 import { InvalidUserPropertyException } from '../exceptions/InvalidUserPropertyException';
 
 export const isValidPassword = async (password: string) => {
-  if (password.length < 10) {
+  if (password.length < MINIMUM_LENGTH_OF_USER_PASSWORD) {
     throw new InvalidUserPropertyException('passwort is too short');
+  }
+
+  if (password.length > MAXIMUM_LENGTH_OF_USER_PASSWORD) {
+    throw new InvalidUserPropertyException('passwort is too long');
   }
 
   return Promise.resolve();

869a77b04fe98b9f2688ce4bceca710995f0fca2
Noah Park <polygon.puma@gmail.com>
2020-10-17 18:19:17 +0200

    add combined reducer to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reducer from './reducers/RootReducer';
 import '@material/react-icon-button/dist/icon-button.css';
 import 'typeface-roboto';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Page } from './Page';
 import ApplicationThemeProvider from './ApplicationThemeProvider';
 import { ApplicationContextProvider } from './ApplicationContextProvider';
+import setup from './reducers/SetupReducer';
+import application from './reducers/ApplicationReducer';
+
+const reducer = combineReducers({
+  application: application,
+  setup: setup,
+});
 
 const store = createStore(reducer, applyMiddleware(logger));
 

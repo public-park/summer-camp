a13fe7f1f8b5965091efec1e519452f39c61fbe4
Noah Park <polygon.puma@gmail.com>
2020-10-20 08:15:52 +0200

    add user pool to activity handler

--- src-server/message-handler/handler/ActivityMessageHandler.ts
+++ src-server/message-handler/handler/ActivityMessageHandler.ts
@@ -1,10 +1,17 @@
 import { ActivityMessage } from '../../models/socket/messages/ActivityMessage';
+import { UserPool } from '../../pool/UserPool';
 import { UserWithOnlineState } from '../../pool/UserWithOnlineState';
 
-const handle = async (user: UserWithOnlineState, message: ActivityMessage): Promise<ActivityMessage> => {
+const handle = async (
+  pool: UserPool,
+  user: UserWithOnlineState,
+  message: ActivityMessage
+): Promise<ActivityMessage> => {
   user.activity = message.payload.activity;
 
   await user.persist();
+
+  pool.broadcastToAll(user);
 
   return new ActivityMessage(user.activity, message.header.id);
 };

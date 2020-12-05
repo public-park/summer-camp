de4a54613789a3e1ec8e2b7cd9e3c1e09681ac15
Noah Park <polygon.puma@gmail.com>
2020-12-05 16:23:16 +0100

    remove phone actions from application reducer

--- src/reducers/ApplicationReducer.ts
+++ src/reducers/ApplicationReducer.ts
@@ -1,8 +1,6 @@
 import { ApplicationStore } from '../store/ApplicationStore';
 import produce from 'immer';
 import { Action } from '../actions/ActionType';
-import { PhoneState } from '../phone/PhoneState';
-import { IS_PHONE_NUMBER_REGEXP, IS_NUMBER_REGEXP } from '../Constants';
 import { DefaultApplicationStore } from '../store/DefaultApplicationStore';
 import { UserWithPresenceDocument } from '../models/documents/UserDocument';
 
@@ -10,75 +8,17 @@ const application = (state: ApplicationStore = DefaultApplicationStore, action: 
   return produce(state, (draft) => {
     switch (action.type) {
       case 'PAGE_LOAD':
-        draft.phone.devices.input = action.payload.input;
-        draft.phone.devices.output = action.payload.output;
-        draft.token = action.payload.token;
-
         if (!action.payload.token) {
           draft.page = 'LOGIN_PAGE';
         }
-
         break;
 
       case 'CONNECTION_STATE_CHANGE':
         draft.connection.state = action.payload.state;
-
-        break;
-
-      case 'PHONE_DISPLAY_UPDATE':
-        if (IS_NUMBER_REGEXP.test(action.payload)) {
-          action.payload = `+${action.payload}`;
-        }
-
-        draft.phone.display = {
-          value: action.payload,
-          isValidPhoneNumber: IS_PHONE_NUMBER_REGEXP.test(action.payload),
-        };
-        break;
-
-      case 'PHONE_ERROR':
-        draft.phone.error = action.payload.message;
-        break;
-
-      case 'PHONE_STATE_CHANGE':
-        if (action.payload === PhoneState.Expired) {
-          draft.phone.token = undefined;
-        }
-
-        if (action.payload !== PhoneState.Error) {
-          draft.phone.error = undefined;
-        }
-
-        draft.phone.state = action.payload;
         break;
 
       case 'CALL_STATE_CHANGE':
         draft.call = action.payload;
-
-        break;
-
-      case 'PHONE_TOKEN_UPDATE':
-        draft.phone.token = action.payload;
-
-        if (action.payload.error) {
-          draft.phone.error = action.payload.error;
-        } else {
-          draft.phone.error = undefined;
-        }
-
-        break;
-
-      case 'PHONE_CONFIGURATION_UPDATE':
-        draft.phone.configuration = action.payload;
-        draft.phone.token = undefined;
-        break;
-
-      case 'PHONE_INPUT_DEVICE_UPDATE':
-        draft.phone.devices.input = action.payload;
-        break;
-
-      case 'PHONE_OUTPUT_DEVICE_UPDATE':
-        draft.phone.devices.output = action.payload;
         break;
 
       case 'AUDIO_DEVICES_EXCEPTION':
@@ -88,14 +28,6 @@ const application = (state: ApplicationStore = DefaultApplicationStore, action: 
       case 'AUDIO_DEVICES_CHANGE':
         draft.devices.audio.input = action.payload.input;
         draft.devices.audio.output = action.payload.output;
-        break;
-
-      case 'PHONE_INPUT_DEVICE_LOST':
-        draft.phone.devices.input = undefined;
-        break;
-
-      case 'PHONE_OUTPUT_DEVICE_LOST':
-        draft.phone.devices.output = undefined;
         break;
 
       case 'WORKSPACE_VIEW':

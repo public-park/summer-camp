e817bed9a904d4e34b7c54f20a3e6bf9771ac11e
Noah Park <polygon.puma@gmail.com>
2020-12-19 18:21:50 +0100

    invalidate phone numbers starting with +0

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
-export const IS_PHONE_NUMBER_REGEXP = /^\+\d+$/;
+export const IS_PHONE_NUMBER_REGEXP = /^\+[1-9]{1}\d+$/;
 export const IS_NUMBER_REGEXP = /^\d+$/;
 export const IS_TWILIO_ACCOUNT_SID_REGEXP = /^AC[0-9abcdef]{32}$/;
 export const IS_TWILIO_API_SECRET_REGEXP = /^[a-z0-9]{32}$/i;

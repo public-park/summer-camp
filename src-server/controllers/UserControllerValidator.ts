65d0b47754b891dc485a9b6f34703087148717c0
Noah Park <polygon.puma@gmail.com>
2021-02-15 21:37:50 +0100

    extend parameter validation

--- src-server/controllers/UserControllerValidator.ts
+++ src-server/controllers/UserControllerValidator.ts
@@ -4,7 +4,11 @@ import { InvalidUserPropertyException } from '../exceptions/InvalidUserPropertyE
 import { UserAlreadyExistsException } from '../exceptions/UserAlreadyExistsException';
 import { UserRole } from '../models/UserRole';
 
-export const isValidTagList = (tags: Array<string>) => {
+const isString = (value: unknown): value is string => {
+  return Object.prototype.toString.call(value) === '[object String]';
+};
+
+export const isValidTagList = (tags: unknown) => {
   if (!Array.isArray(tags)) {
     throw new InvalidUserPropertyException('tags it not type array');
   }
@@ -14,19 +18,31 @@ export const isValidTagList = (tags: Array<string>) => {
   }
 };
 
-export const isValidName = async (name: string) => {
+export const isValidName = async (name: unknown) => {
+  if (!isString(name)) {
+    throw new InvalidUserPropertyException('name it not type string');
+  }
+
   if (await userRepository.getByName(name)) {
     throw new UserAlreadyExistsException();
   }
 };
 
-export const isValidActivity = (activity: string) => {
+export const isValidActivity = (activity: unknown) => {
+  if (!isString(activity)) {
+    throw new InvalidUserPropertyException('activity it not type string');
+  }
+
   if (!Object.values(UserActivity).includes(<UserActivity>activity)) {
     throw new InvalidUserPropertyException('activity is unknown');
   }
 };
 
-export const isValidRole = (role: string) => {
+export const isValidRole = (role: unknown) => {
+  if (!isString(role)) {
+    throw new InvalidUserPropertyException('role it not type string');
+  }
+
   if (!Object.values(UserRole).includes(<UserRole>role)) {
     throw new InvalidUserPropertyException('role is unknown');
   }
